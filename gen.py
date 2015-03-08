c8c1ff4bc2e409f9047b6a6e1b601455e0b5934b
Mitch LeBlanc <supermitch@gmail.com>
2015-03-07 21:37:22 -0800

    White noise working

--- gen.py
+++ gen.py
@@ -1,3 +1,4 @@
+import random
 import sys
 
 import pygame
@@ -6,6 +7,9 @@ from pygame.locals import *
 def gen_map(width=500, height=500):
     """ Returns a map of width x height tiles. """
     map = [[None for x in range(width)] for y in range(height)]
+    for row in range(height):
+        for col in range(width):
+            map[row][col] = random.randint(1, 255)
     return map
 
 def render(map):
@@ -14,6 +18,15 @@ def render(map):
     surf = pygame.display.set_mode(screen_size, RESIZABLE)
     pygame.display.set_caption('Pyland Gen 1.0')
     surf.fill(BG_COLOR)
+    for i, row in enumerate(map):
+        for j, col in enumerate(row):
+            px = pygame.Surface((1, 1))
+            shade = map[i][j]
+            color = (shade, shade, shade)
+            px.fill(color)
+            pos = i, j
+            surf.blit(px, pos)
+
     pygame.display.flip()
 
 def get_input():
