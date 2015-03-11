8667ca8e320841dbb2a68460e3116307f63e523c
Mitch LeBlanc <supermitch@gmail.com>
2015-03-10 23:00:21 -0700

    Start generating aaline points
    Draw polygon to bound island from aaline pnoise points

--- gen.py
+++ gen.py
@@ -1,3 +1,4 @@
+import math
 import random
 import sys
 
@@ -16,31 +17,27 @@ def gen_border():
     for i in range(points):
         x = float(i) * span / points
         y = pnoise1(x + base, octaves)
-        border.append(y)
+        border.append((i, y))
     print(len(border))
     return border
 
 def gen_shore(border, radius=300, scale=75):
     """ Apply our border to our island. """
-    return [radius + y * scale for y in border]
+    radii = (radius + y * scale for _, y in border)
+    angles = (x * math.pi / 180.0 for x, _ in border)
+    points = [(radius * math.cos(theta), radius * math.sin(theta)) \
+             for radius, theta in zip(radii, angles)]
+    return points
 
-def gen_map(width=500, height=500):
+def gen_random_map(width=500, height=500):
     """ Returns a map of width x height tiles. """
     # Initialize map to matrix of None
     map = [[None for x in range(width)] for y in range(height)]
-
-    octaves = 2
-    freq = 16.0 * octaves
     for row in range(height):
         for col in range(width):
-            # Choose a random color, for now
+            # Random R, G & B
             map[row][col] = (random.randint(0, 255), random.randint(0, 255),
                              random.randint(0, 255))
-            # snoise?
-            #shade1 = int(pnoise2(row/freq, col/freq, octaves) * 127.0 + 128.0)
-            #shade2 = int(snoise2(row/freq, col/freq, octaves) * 127.0 + 128.0)
-            #shade = (shade1 + shade2) % 255
-            #map[row][col] = (shade, shade, shade)
     return map
 
 def render(map):
@@ -84,7 +81,8 @@ def main():
     print('Pyland Gen 1.0')
     pygame.init()
     border = gen_border()
-    map = gen_map()
+    point_list = gen_shore(border)
+    map = gen_random_map()
     render(map)
     while True:
         get_input()
