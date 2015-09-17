cf2c299fd8a73e9f02d6f5732a945c373e17dfa3
Mitch LeBlanc <supermitch@gmail.com>
2015-09-16 20:52:29 -0700

    Add Island.cells_to_tiles and .normalize

--- island.py
+++ island.py
@@ -1,11 +1,35 @@
+from __future__ import division
+
+from tile import Tile
+
 
 class Island(object):
 
-    def __init__(self):
+    def __init__(self, width=900, height=900):
         self.radius = None
         self.shore_noise = None
         self.rect_shore = None
         self.shore_lines = None
         self.peak = None
         self.spokes = None
+        self.tiles = [[None] * width for _ in range(height)]
+
+    def cells_to_tiles(self, *cells):
+        """
+        Apply a Cell(x, y, z) into an Island tile height.
+        """
+        for x, y, z in cells:
+            self.tiles[x][y] = Tile(x, y, z)
+
+    def normalize(self):
+        max_height = 1
+        for row in self.tiles:
+            for tile in row:
+                if tile is not None:
+                    if tile.height > max_height:
+                        max_height = tile.height
+        for row in self.tiles:
+            for tile in row:
+                if tile is not None:
+                    tile.height = float(tile.height) / max_height
 
