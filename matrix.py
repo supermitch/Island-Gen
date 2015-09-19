7c7aaee1a996078e0f07be8b2cb09ebfd1590689
Mitch LeBlanc <supermitch@gmail.com>
2015-09-18 21:26:10 -0700

    Add matrix functions and tests
    Make separate functions for get_offsets and new arrays, find_neighbours is not complete.

---
+++ matrix.py
@@ -0,0 +1,34 @@
+from __future__ import division
+
+import itertools
+
+
+def get_offsets(span):
+    """
+    Get matrix offsets for a square of distance `span`.
+    """
+    if span < 0:
+        raise ValueError('Cannot return neighbours for negative distance')
+
+    all_offsets = set(itertools.product([x for x in range(-span, span + 1)], repeat=2))
+    if span > 1:
+        inner_offsets = set(itertools.product([x for x in range(-(span - 1), span)], repeat=2))
+    else:
+        inner_offsets = set()
+    return all_offsets - inner_offsets
+
+
+def find_neighbours_2D(array, start, span):
+    """
+    Return neighbours in a 2D array, given a start point and range.
+    """
+    x, y = start  # Start coords
+    rows = len(array)  # How many rows
+    cols = len(array[0])  # Assume square matrix
+    return
+
+
+def new(size, value=None):
+    """ Initialize a new square matrix. """
+    return [[value] * size for _ in range(size)]
+
---
+++ tests/matrix_test.py
@@ -0,0 +1,29 @@
+from __future__ import division
+
+import matrix
+
+
+def test_get_offsets():
+    assert matrix.get_offsets(0) == set([(0, 0)])
+    assert matrix.get_offsets(2) == set([(1, 2), (-2, 1), (-2, 0), (-2, 2),
+                                         (-1, 2), (2, -2), (2, 1), (2, -1),
+                                         (2, 0), (-2, -1), (-1, -2), (-2, -2),
+                                         (2, 2), (0, -2), (1, -2), (0, 2)])
+
+def test_new():
+    m = matrix.new(3, 'a')
+    assert len(m) == 3  # 3 rows
+    assert len(m[0]) == 3  # 3 cols
+    assert m[0][0] == 'a'
+    assert m[2][2] == 'a'
+
+    # Ensure each element of matrix is unique
+    m[1][0] == 'c'
+    assert m[1][1] == 'a'
+    assert m[2][0] == 'a'
+
+def test_find_neighbours_2D():
+    m = matrix.new(4)
+    results = matrix.find_neighbours_2D(m, (2, 2), 1)
+    assert results == []
+
