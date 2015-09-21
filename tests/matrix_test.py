b2e38db9f562af6bdd9df2dc060967d6f8f6eaa7
Mitch LeBlanc <supermitch@gmail.com>
2015-09-20 21:57:55 -0700

    Add test for bound trimming find_neighbours_2D

--- tests/matrix_test.py
+++ tests/matrix_test.py
@@ -30,3 +30,7 @@ def test_find_neighbours_2D():
     assert results == set([(1, 2), (3, 2), (1, 3), (3, 3),
                            (3, 1), (2, 1), (2, 3), (1, 1)])
 
+    results = matrix.find_neighbours_2D(m, (1, 1), 2)
+    assert results == set([(0, 3), (1, 3), (2, 3), (3, 3),
+                           (3, 2), (3, 1), (3, 0)])
+
